707bab8eed2c67478b9f69e80984100233ac1955
leek922 <132716335+leek922@users.noreply.github.com>
2025-06-20 10:48:29 -0400

    Add files via upload

---
+++ boilerplates/main.py
@@ -0,0 +1,21 @@
+"""
+This is a template for a Flask file for developing your web apps. 
+Feel free to fork and clone this to modify as needed, or if you prefer, start from scratch on a new file.
+
+Please ensure that this file is located in the main directory of your project.
+"""
+
+from flask import Flask, render_template # imports
+
+app = Flask(__name__) # create Flask app
+
+
+
+
+
+"""
+Don't delete the code below! Needed for running the app.
+"""
+
+if __name__ == "__main__":
+    app.run()
