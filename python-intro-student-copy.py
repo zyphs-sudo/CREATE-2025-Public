894f10bec52cba831340544bf6b3bcad2e1fe2f4
leek922 <132716335+leek922@users.noreply.github.com>
2025-06-20 10:57:44 -0400

    Add files via upload

---
+++ python-intro-student-copy.py
@@ -0,0 +1,63 @@
+# Welcome to Think, Code, Create: Software Engineering with Python & AI
+# In this course you will learn how to develop web applications using python tools 
+# and accelerate your journey as a web developer! 
+
+# This activity introduces you to the fundamentals of programming in Python, you will 
+# learn some key concepts, like if statements, loops, functions, variables and more! 
+# Throughout this activity, you will get a chance to apply the concepts you learned in 
+# lecture and build your programming skills! 
+
+# Activity 1.1: Given a number, output if the number is even or odd. 
+# For odd numbers, add a print statement saying that the number is odd. 
+# For even numbers, add a print statement saying that the number is even. 
+# You can assume that the input is a valid integer number between 0 and 100. 
+# Hint: look up the 'mod' function. 
+# Below we have a line of code where we
+# are collecting data from the user and storing it in the variable called x. 
+# We are converting it to an integer for to make it easy for you. 
+
+x = int(input("Please type in a number: "))
+
+
+# Activity 1.2: The previous code only runs once and then terminates. 
+# How can we make it run continuously? 
+# Hint: What is something we discussed in lecture that allows you to repeat code?
+
+
+
+# Activity 1.3: Turn the code into a function, so you can run the code selectively. 
+# How can we turn code into function? 
+
+
+
+# Activity 1.4: Now we want to design a loop that terminates on its own. 
+# Create a loop that counts from 0 to 5 and terminates on its own. 
+
+
+
+
+# Activity 1.5: How can we do this with a for loop instead of a while loop? 
+
+
+
+
+# Activity 1.6: Design a counter that counts within a range, for example, 10 - 20. 
+# But we want to allow that range to be custom. How do we do that?
+# Hint: Do you remember function parameters and arguments? 
+
+
+
+
+# Activity 1.7: Design a counter that counts within a custom range, but 
+# only prints the even numbers? Try to reuse as much of you existing 
+# code as possible. 
+# Hint: you can call a function within another function. 
+
+
+
+
+
+# Activity 1.8: Storing data in your code. Design a function that outputs 
+# the even and odd numbers within a given range, but all in one line like so: 
+# [10, 12, 14, 16, 18]
+# [11, 13, 15, 17, 19]
