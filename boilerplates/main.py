0226608a35d9fc6e0b4afdd82c5c15340be3959d
leek922 <132716335+leek922@users.noreply.github.com>
2025-06-20 10:51:48 -0400

    Update main.py

--- boilerplates/main.py
+++ boilerplates/main.py
@@ -3,6 +3,9 @@ This is a template for a Flask file for developing your web apps.
 Feel free to fork and clone this to modify as needed, or if you prefer, start from scratch on a new file.
 
 Please ensure that this file is located in the main directory of your project.
+
+To run, cd into your project directory and then run the following command:
+flask --app (name of your project) run --debug
 """
 
 from flask import Flask, render_template # imports
