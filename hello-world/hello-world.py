d2c325f1036ce4a434e5e695647bfb5f1aad4c3c
Prarthona Paul <p.paul@mail.utoronto.ca>
2025-07-07 12:50:28 -0400

    added boilerplate for helloworld activity

---
+++ hello-world/hello-world.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Activity 4.1: basis Hello World Web app
+
+
+# Activity 4.2: Use routes, using routes to edit format
+
+
+# Activity 4.3 Introduce --debug to allow refresh on change 
+
+# Activity 4.4: rendering HTML templates
+
+
+# Activity 4.5: 
+
+if __name__ == '__main__':
+    app.run(debug=True)
