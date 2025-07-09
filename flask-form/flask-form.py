c06a90e58df3f18fb29355c0f33dc678bf55dc8b
Prarthona Paul <p.paul@mail.utoronto.ca>
2025-07-09 08:55:59 -0400

    change activity to share html css instead of python

--- flask-form/flask-form.py
+++ flask-form/flask-form.py
@@ -1,10 +1,7 @@
 from flask import (
     Flask, 
     render_template, 
-    request, 
-    flash, 
-    redirect, 
-    url_for,
+    request,
 )
 
 app = Flask(__name__)
@@ -22,41 +19,15 @@ login_info = {
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == "POST":
-        if request.form["username"] in login_info.keys() and login_info[request.form["username"]] == request.form["password"]:
-            app.config["USERNAME"] = request.form["username"]
-            app.config["PASSWORD"] = request.form["password"]
-            return f"""<h1>Welcome to the Flask Form, {app.config["USERNAME"]}!</h1>
-            <button onclick="location.href='/'">Go to Home</button>
-            """
-        else:
-            error = "Invalid username or password"
-    return render_template("login.html", error=error)
+    
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    error = None
-    if request.method == 'POST':
-        if request.form["username"] in login_info.keys():
-            error = "Username already exists"
-        elif request.form["password"] == "":
-            error = "Password cannot be empty"
-        elif request.form["username"] == "" or request.form["password"] == "":
-            error = "Username and password cannot be empty"
-        else: 
-            login_info[request.form["username"]] = request.form["password"]
-            app.config["USERNAME"] = request.form["username"]
-            app.config["PASSWORD"] = request.form["password"]
-            return f"""
-            <h1>Registration successful! Welcome, {app.config["USERNAME"]}!</h1>
-            <button onclick="location.href='/'">Go to Home</button>
-            """
-    return render_template('register.html', register_error=error)
+    
 
 if __name__ == "__main__":
     app.run()
